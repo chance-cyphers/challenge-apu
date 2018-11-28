25f29aeb9c09ac79697e3320feea4f0a80480855
chance <chance.a.cyphers@gmail.com>
2018-11-28 12:57:36 -0500

    Deploy to GKE with deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ git pull
 
 docker build -t skyfrog28/challenge:latest .
 docker push skyfrog28/challenge:latest
+
+kubectl set image deployment challenge-api=skyfrog28/challenge:latest
--- main.go
+++ main.go
@@ -38,7 +38,7 @@ import (
 type server struct{}
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello BOB"}, nil
+	return &pb.HelloReply{Message: "Hello LOB"}, nil
 }
 
 func main() {
