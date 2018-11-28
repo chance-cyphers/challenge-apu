bc52f3189b53e0c61be76af6dba0f2743bddadf6
chance <chance.a.cyphers@gmail.com>
2018-11-28 14:17:48 -0500

    Remove comments

--- main.go
+++ main.go
@@ -11,30 +11,6 @@ import (
 	"os"
 )
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//	sess, er := session.NewSession(&aws.Config{
-//		Region: aws.String("us-east-1")},
-//	)
-//
-//	if er != nil {
-//		exitErrorf("error creating session", er)
-//	}
-//
-//	svc := s3.New(sess)
-//
-//	s3Req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
-//		Bucket: aws.String("challenge-video-bucket"),
-//		Key:    aws.String("/test.txt"),
-//	})
-//
-//	urlStr, er := s3Req.Presign(15 * time.Minute)
-//	if er != nil {
-//		exitErrorf("error presigning", er)
-//	}
-//
-//	fmt.Fprintf(w, "{ \"url\": \"%s\"}", urlStr)
-//}
-
 type server struct{}
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
@@ -57,8 +33,6 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-	//http.HandleFunc("/", handler)
-	//log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func exitErrorf(msg string, args ...interface{}) {
