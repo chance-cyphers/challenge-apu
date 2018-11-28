3d975a6c4a90297e735ff8a8b8b9d009c23aa0a6
chance <chance.a.cyphers@gmail.com>
2018-11-28 16:49:04 -0500

    Do some cloud storage stuff

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"cloud.google.com/go/storage"
 	"context"
+	"fmt"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/reflection"
@@ -13,7 +17,38 @@ import (
 type server struct{}
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello LOB"}, nil
+	message, err := getBuckets()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return &pb.HelloReply{Message: message}, err
+}
+
+func getBuckets() (string, error) {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	projectID := "challenge-api"
+	it := client.Buckets(ctx, projectID)
+	buffer := bytes.Buffer{}
+	for {
+		battrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return "", err
+		}
+		_, _ = buffer.WriteString(battrs.Name)
+		_, _ = buffer.WriteString("\n")
+	}
+	return buffer.String(), nil
 }
 
 func main() {
@@ -29,7 +64,10 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterGreeterServer(grpcServer, &server{})
 	reflection.Register(grpcServer)
+
+	fmt.Println("starting server on port " + port)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
 }
