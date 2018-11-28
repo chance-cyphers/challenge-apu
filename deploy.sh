2d3df03ecd414d83abab08963ed58692312d3773
chance <chance.a.cyphers@gmail.com>
2018-11-28 14:08:04 -0500

    Add version bumping

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 set -ex
 
+# Be up to date
 git pull
 
+# Bump version
+version=`cat VERSION`
+major="$(cut -d'.' -f1 <<< $version)"
+minor="$(cut -d'.' -f2 <<< $version)"
+newVersion="$major.$((minor+1))"
+echo newVersion > VERSION
+
+# Commit version bump
+git add -A
+git commit -m "Version $version"
+git push
+
+# Build/store image
 docker build -t skyfrog28/challenge:latest .
 docker push skyfrog28/challenge:latest
 
+# Put it up there
 kubectl set image deployment challenge-api challenge-api=skyfrog28/challenge:latest
