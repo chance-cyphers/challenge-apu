46918be2049c560fc92cfedbe478b8393b28a5ce
chance <chance.a.cyphers@gmail.com>
2018-11-28 13:51:56 -0500

    Deploy to GKE with deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -ex
 
 git pull
@@ -5,4 +6,4 @@ git pull
 docker build -t skyfrog28/challenge:latest .
 docker push skyfrog28/challenge:latest
 
-kubectl set image deployment challenge-api=skyfrog28/challenge:latest
+kubectl set image deployment challenge-api challenge-api=skyfrog28/challenge:latest
