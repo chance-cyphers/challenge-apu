502540bd1ccf52f1a9235747181e6ab7a55513c0
chance <chance.a.cyphers@gmail.com>
2018-12-21 13:11:53 -0500

    Setup endpoints to transcode json -> grpc

--- util/compile-proto.sh
+++ util/compile-proto.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 protoc \
+    -I./proto \
     --include_imports \
     --include_source_info \
-    --proto_path=./proto \
     --descriptor_set_out=api_descriptor.pb \
     --go_out=plugins=grpc:proto \
     Challenge.proto
